bd845af9168657386476830829f181d4c6ff7b7e
Romulo Sousa <romulo@labsit.io>
2024-10-14 21:33:27 -0300

    feat: add new route to search books by publisher

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,6 +4,7 @@ import BookController from "../controllers/BookController.js";
 const routerBook = express.Router();
 
 routerBook.get("/books", BookController.getBooks);
+routerBook.get("/books/search", BookController.getBooksByPublisher);
 routerBook.get("/books/:id", BookController.getBookById);
 routerBook.post("/books", BookController.createBook);
 routerBook.put("/books/:id", BookController.updateBook);
